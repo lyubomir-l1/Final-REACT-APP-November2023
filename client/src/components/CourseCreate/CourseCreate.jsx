a13017ff41e2d8edbf9f7da27c2b46fa7df27e7f
Lyubomir Lyubenov <lyubomir2017@gmail.com>
2023-11-23 23:45:04 +0200

    Update Create Form

--- client/src/components/CourseCreate/CourseCreate.jsx
+++ client/src/components/CourseCreate/CourseCreate.jsx
@@ -1,9 +1,30 @@
 import { useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 import * as courseService from '../../services/courseService';
 
+// const formInitialState = {
+//     title: '',
+//     category: '',
+//     maxLevel: '',
+//     trainer: '',
+//     imageUrl: '',
+//     summary: '',
+// };
+
+
 export default function CourseCreate() {
+
+
+    // const [formValues, setFormValues] = useState(formInitialState);
     const navigate = useNavigate();
+
+    // const changeHandler = (e) => {
+    //     e.preventDefault();
+    //     setFormValues(state => ({
+    //         ...state,
+    //         [e.target.name]: e.target.value,
+    //     })); 
+    // }
     
     const createCourseSubmitHandler = async (e) => {
         e.preventDefault();
@@ -12,8 +33,8 @@ export default function CourseCreate() {
 
         try {
             await courseService.create(courseData);
-
             navigate('/courses');
+
         } catch (err) {
             // Error notification
             console.log(err);
@@ -22,27 +43,27 @@ export default function CourseCreate() {
 
     return (
         <section id="create-page" className="auth">
-            <form id="create" onSubmit={createCourseSubmitHandler}>
+            <form onSubmit={createCourseSubmitHandler} id="create" >
                 <div className="container">
                     <h1>Create Course</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" placeholder="Enter game title..." />
+                    <input type="text" id="title" name="title" /*value={formValues.title} onChange={changeHandler} */ placeholder="Enter game title..." />
 
                     <label htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" placeholder="Enter game category..." />
+                    <input type="text" id="category" name="category" /* value={formValues.category} onChange={changeHandler} */ placeholder="Enter game category..." />
 
                     <label htmlFor="levels">MaxLevel:</label>
-                    <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
+                    <input type="number" id="maxLevel" name="maxLevel" /* value={formValues.maxLevel} onChange={changeHandler} */ min="1" placeholder="1" />
 
                     <label htmlFor="levels">Trainer:</label>
-                    <input type="text" id="trainer" name="trainer" placeholder="Trainer name" />
+                    <input type="text" id="trainer" name="trainer" /* value={formValues.trainer} onChange={changeHandler} */ placeholder="Trainer name" />
 
                     <label htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+                    <input type="text" id="imageUrl" name="imageUrl" /* value={formValues.imageUrl} onChange={changeHandler} */ placeholder="Upload a photo..." />
 
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary"></textarea>
-                    <input className="btn submit" type="submit" value="Create Course" />
+                    <textarea name="summary" /* value={formValues.summary} onChange={changeHandler} */ id="summary"></textarea>
+                    <input className="btn submit" type="submit"  value="Create Course" />
                 </div>
             </form>
         </section>
